55692d3fea24e2acbc81fddeef86cf0f38c2eb6d
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2024-03-31 00:19:05 -0300

    feat: added bun and radar chart

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
-import { computed, defineProps, onMounted, reactive, ref, watch } from 'vue'
+import {  defineProps, reactive, ref, watch } from 'vue'
 import pokeDex from '../assets/pokedex.png'
 import heroMedia from './heroMedia.vue';
+import {
+    Chart as ChartJS,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import { Radar } from 'vue-chartjs'
+
+ChartJS.register(
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+)
 
 const propHero = defineProps({
     heroData: {
@@ -15,21 +34,85 @@ const heroImage = ref();
 const heroVideo = ref();
 
 watch(() => propHero, (newValue, oldValue) => {
-    handleMedia(newValue.heroData?.name)
-}, {deep: true })
+    handleMedia(newValue.heroData?.name);
+    handleRadar(newValue.heroData);
+}, { deep: true })
 
 
-const handleMedia = (hero_name: string) =>{
+const handleMedia = (hero_name: string) => {
     let videoHeroName = hero_name.replace("npc_dota_hero_", "");
     heroImage.value = videoHeroName ? `https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${videoHeroName}.png` : '';
     heroVideo.value = videoHeroName ? `https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${videoHeroName}.webm` : '';
 }
 
+const handleRadar = (heroStat) => {
+    chartData.value.datasets[0].data =
+        [
+            parseInt(heroStat?.base_health),
+            parseInt(heroStat?.base_mana),
+            parseInt(heroStat?.attack_range),
+            parseInt(heroStat?.base_attack_min),
+            parseInt(heroStat?.base_armor),
+            parseInt(heroStat?.move_speed),
+            parseInt(heroStat?.base_mr)
+        ];
+}
+
 
 let videoHeroName = hero?.heroData?.name.replace("npc_dota_hero_", "");
-    heroImage.value = videoHeroName ? `https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${videoHeroName}.png` : '';
-    heroVideo.value = videoHeroName ? `https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${videoHeroName}.webm` : '';
+heroImage.value = videoHeroName ? `https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${videoHeroName}.png` : '';
+heroVideo.value = videoHeroName ? `https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${videoHeroName}.webm` : '';
 
+
+const chartData = ref({
+    labels: [
+        'HEALTH',
+        'MANA',
+        'ATTACK RANGE',
+        'BASE ATTACK',
+        'BASE ARMOR',
+        'MOVE SPEED',
+        'MAGIC RESISTANCE'
+    ],
+    datasets: [
+        {
+            label: '',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            pointBackgroundColor: 'rgba(255,99,132,1)',
+            pointBorderColor: '#fff',
+            pointHoverBackgroundColor: '#fff',
+            pointHoverBorderColor: 'rgba(255,99,132,1)',
+            data: []
+        }
+    ]
+})
+
+const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    label: {
+        display: false
+    },
+    scales: {
+        r: {
+            grid: {
+                color: 'gray'
+            },
+            angleLines: {
+                color: 'rgba(240, 240, 240,0.5)',
+            },
+            pointLabels: {
+                color: 'white'
+            },
+            ticks: {
+                color: 'white',
+                backdropColor: '#000'
+            }
+        }
+    },
+
+}
 
 </script>
 
@@ -47,10 +130,21 @@ let videoHeroName = hero?.heroData?.name.replace("npc_dota_hero_", "");
             </div>
         </div>
 
+        <div class="absolute max-h-80 top-[220px] left-[440px]">
+            <Radar :data="chartData" :options="options" :key="propHero?.heroData?.id" />
+        </div>
+
     </div>
-
-
 
 </template>
 
-<style scoped></style>
+<style scoped>
+.chart-container {
+    position: relative;
+    height: 90vh;
+}
+
+canvas {
+    background: #163355;
+}
+</style>
